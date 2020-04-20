de51ba632b2e1be0b3b7b17fe85146d705ddc274
song geng <827265717@qq.com>
2020-04-20 03:29:46 -0400

    Update analyzer.py

--- AI API/analyzer.py
+++ AI API/analyzer.py
@@ -1,17 +1,17 @@
-def patient_alarm(dictionary):
+def analyzer(dict):
     result = []
-    if dictionary['Pulse'] > 60 or dictionary['Pulse'] < 20:
-        if dictionary['Pulse'] > 60:
+    if dict['Pulse'] > 60 or dict['Pulse'] < 20:
+        if dict['Pulse'] > 60:
             result.append('Note: High Pulse Alarm!')
         else:
             result.append('Note: Low Pulse Alarm!')
-    if dictionary['Blood_Pressure'] > 140 or dictionary['Blood_Pressure'] < 80:
-        if dictionary['Blood_Pressure'] > 140:
+    if dict['BloodPressure'] > 140 or dict['BloodPressure'] < 80:
+        if dict['BloodPressure'] > 140:
             result.append('Note: High Blood Pressure Alarm!')
         else:
             result.append('Note: Low Blood Pressure Alarm!')
-    if dictionary['Oxygen_Level'] > 110 or dictionary['Oxygen_Level'] < 60:
-        if dictionary['Oxygen_Level'] > 110:
+    if dict['BloodOxygen'] > 110 or dict['BloodOxygen'] < 60:
+        if dict['BloodOxygen'] > 110:
             result.append('Note: High Blood Oxygen Alarm!')
         else:
             result.append('Note: Low Oxygen Level Alarm!')
@@ -19,15 +19,14 @@ def patient_alarm(dictionary):
         result.append('You look healthy!')
     return result
 
-def main():
-    print(patient_alarm({'Pulse': 50, 'Blood_Pressure': 100, 'Oxygen_Level': 120}))
-    print(patient_alarm({'Pulse': 120, 'Blood_Pressure': 60, 'Oxygen_Level': 90}))
-    print(patient_alarm({'Pulse': 30, 'Blood_Pressure': 100, 'Oxygen_Level': 30}))
-    print(patient_alarm({'Pulse': 50, 'Blood_Pressure': 10, 'Oxygen_Level': 100}))
-    print(patient_alarm({'Pulse': 50, 'Blood_Pressure': 100, 'Oxygen_Level': 120}))
-    print(patient_alarm({'Pulse': 120, 'Blood_Pressure': 60, 'Oxygen_Level': 30}))
-    print(patient_alarm({'Pulse': 30, 'Blood_Pressure': 30, 'Oxygen_Level': 120}))
-    print(patient_alarm({'Pulse': 100, 'Blood_Pressure': 70, 'Oxygen_Level': 100}))
-
 if __name__ == '__main__':
-    main()
+    print(analyzer({'Pulse': 50, 'Blood_Pressure': 100, 'Oxygen_Level': 120}))
+    print(analyzer({'Pulse': 120, 'Blood_Pressure': 60, 'Oxygen_Level': 90}))
+    print(analyzer({'Pulse': 30, 'Blood_Pressure': 100, 'Oxygen_Level': 30}))
+    print(analyzer({'Pulse': 50, 'Blood_Pressure': 10, 'Oxygen_Level': 100}))
+    print(analyzer({'Pulse': 50, 'Blood_Pressure': 100, 'Oxygen_Level': 120}))
+    print(analyzer({'Pulse': 120, 'Blood_Pressure': 60, 'Oxygen_Level': 30}))
+    print(analyzer({'Pulse': 30, 'Blood_Pressure': 30, 'Oxygen_Level': 120}))
+    print(analyzer({'Pulse': 100, 'Blood_Pressure': 70, 'Oxygen_Level': 100}))
+    print(analyzer({'Pulse': 42, 'Blood_Pressure': 105, 'Oxygen_Level': 97}))
+
