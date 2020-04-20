8c1be9e1ed25fcf1086194ff7ff41b68e6ae7257
song geng <827265717@qq.com>
2020-04-20 03:26:30 -0400

    Update predictor.py

--- AI API/predictor.py
+++ AI API/predictor.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import numpy as np
 
-def Predict(Pulse,BloodPressure,BloodOxygen):
-    return np.mean(Pulse), np.mean(BloodPressure),np.mean(BloodOxygen)
-
 def Predictor(file):
-    fileIn = pd.read_csv(file+'.csv')
-    Pulse = fileIn.iloc[:,0].values
-    BloodPressure = fileIn.iloc[:,1].values
-    BloodOxygen = fileIn.iloc[:,2].values
-    a,b,c = Predict(Pulse,BloodPressure,BloodOxygen)
+    fileIn = pd.read_csv(file + '.csv')
+    Pulse = fileIn.iloc[:, 0].values
+    BloodPressure = fileIn.iloc[:, 1].values
+    BloodOxygen = fileIn.iloc[:, 2].values
     
-    dict = {}
-    dict["Pulse"] = int(a)
-    dict["BloodPressure"] = int(b)
-    dict["BloodOxygen"] = int(c)
-    return dict
+    dic = dict()
+    dic['Pulse'] = int(np.mean(Pulse))
+    dic['BloodPressure'] = int(np.mean(BloodPressure))
+    dic['BloodOxygen'] = int(np.mean(BloodOxygen))
+    return dic
 
-print(Predictor("data"))
-
-if __name__ == 'main':
-    dict_precition = Predictor("data")
+def main():
+    dict1 = Predictor("data")
+    print(dict1)
+main()
